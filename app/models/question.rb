ee9aae1c279499613a556619f358c60b9e4b914d
Shusuke Ishige <stonhair@gmail.com>
2017-05-17 09:14:14 +0900

    add validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ApplicationRecord
+  validates_presence_of :body, :choice1, :choice2, :choice3, :choice4, :correct_answer
+  validates :correct_answer, numericality: {
+      greater_than_or_equal_to: 1, less_than_or_equal_to: 4
+  }
 end
